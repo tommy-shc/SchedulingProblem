84e8b4fbf3a079c14edbb2723b54f515322d14ec
tommy-shc <tommywei0511@gmail.com>
2025-05-06 11:55:58 -0400

    implemented basic car charging schedule

--- test.py
+++ test.py
@@ -1,14 +1,37 @@
 import simpy
 
-class car(self,env):
-    def __init__(self,env):
+class Car(object):
+    def __init__(self,env,bcs,name):
 
         self.env = env
-        self.action = env.process()
+        self.name = name
+        self.action = env.process(self.run())
+        self.bcs = bcs
 
-    def run(s)
-    
+    def run(self):
+        while True:
+
+            print(self.name," is trying to charge at", self.env.now)
+
+            with bcs.request() as req:
+                yield req
+                print(self.name, "found a charger, currently charging at ",self.env.now)
+                yield(self.env.process(self.charge(5)))
+
+            print(self.name,'done charging, now driving at',self.env.now)
+            trip_duration = 2
+            yield(self.env.timeout(trip_duration))
+                  
+    def charge(self,duration):
+        yield self.env.timeout(duration)
+
+
 
 env = simpy.Environment()
-env.process(car(env))
-env.run(until=15)
+bcs = simpy.Resource(env,capacity=2)
+
+testCar = Car(env,bcs,"one")
+testCar2 = Car(env,bcs,"two")
+testCar3 = Car(env,bcs,"three")
+
+env.run(until=30)
