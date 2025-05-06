a0a55c6c0f74eeb2c8af1843c79fd3a966ab656a
tommy-shc <tommywei0511@gmail.com>
2025-05-06 11:22:46 -0400

    Added simpyTest

---
+++ test.py
@@ -0,0 +1,14 @@
+import simpy
+
+class car(self,env):
+    def __init__(self,env):
+
+        self.env = env
+        self.action = env.process()
+
+    def run(s)
+    
+
+env = simpy.Environment()
+env.process(car(env))
+env.run(until=15)
